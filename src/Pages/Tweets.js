23eb21b4b9142a89b10be535ed2aa59aadebc1e9
kosmos992 <yechan5870@gmail.com>
2022-09-27 11:51:57 +0900

    bundled twittler with webpack

---
+++ src/Pages/Tweets.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import Footer from '../Footer';
+import Tweet from '../Components/Tweet';
+import './Tweets.css';
+import dummyTweets from '../static/dummyData';
+// dummyTweets = [{...},{...},{...}] + {...} < --- new tweet
+
+const getRandomNumber = (min, max) => {
+  return parseInt(Math.random() * (Number(max) - Number(min) + 2));
+};
+
+const Tweets = () => {
+  // TODO : 새로 트윗을 작성하고 전송할 수 있게 useState를 적절히 활용하세요.
+  const [user, setUser] = useState('parkhacker');
+  const [msg, setMsg] = useState('');
+
+  const [tweets, setTweets] = useState([...dummyTweets]);
+
+  const handleChangeTweets = newTweet => {
+    const newTweets = [...tweets];
+    newTweets.unshift(newTweet);
+    setTweets(newTweets);
+  };
+
+  const handleButtonClick = () => {
+    const tweet = {
+      id: tweets.length + 1,
+      username: user,
+      picture: `https://randomuser.me/api/portraits/women/${getRandomNumber(
+        1,
+        98
+      )}.jpg`,
+      content: msg,
+      createdAt: '2019-02-24T16:17:47.000Z',
+      updatedAt: '2019-02-24T16:17:47.000Z',
+    };
+    // TODO : Tweet button 엘리먼트 클릭시 작동하는 함수를 완성하세요.
+    // 트윗 전송이 가능하게 작성해야 합니다.
+    handleChangeTweets(tweet);
+  };
+
+  const handleChangeUser = event => {
+    // TODO : Tweet input 엘리먼트에 입력 시 작동하는 함수를 완성하세요.
+    setUser(event.target.value);
+  };
+
+  const handleChangeMsg = event => {
+    // TODO : Tweet textarea 엘리먼트에 입력 시 작동하는 함수를 완성하세요.
+    setMsg(event.target.value);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="tweetForm__container">
+        <div className="tweetForm__wrapper">
+          <div className="tweetForm__profile">
+            <img src="https://randomuser.me/api/portraits/men/98.jpg" />
+          </div>
+          <div className="tweetForm__inputContainer">
+            <div className="tweetForm__inputWrapper">
+              <div className="tweetForm__input">
+                <input
+                  type="text"
+                  defaultValue="parkhacker"
+                  placeholder="your username here.."
+                  className="tweetForm__input--username"
+                  onChange={handleChangeUser}
+                ></input>
+                <textarea
+                  className="tweetForm__input--message"
+                  onChange={handleChangeMsg}
+                ></textarea>
+              </div>
+              <div className="tweetForm__count" role="status">
+                <span className="tweetForm__count__text">
+                  {/* TODO : 트윗 총 개수를 보여줄 수 있는 Counter를 작성하세요. */}
+                  {'total: ' + tweets.length}
+                </span>
+              </div>
+            </div>
+            <div className="tweetForm__submit">
+              <div className="tweetForm__submitIcon"></div>
+              <button
+                className="tweetForm__submitButton"
+                onClick={handleButtonClick}
+              >
+                Tweet
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="tweet__selectUser"></div>
+      <ul className="tweets">
+        {tweets.map(tweet => (
+          <Tweet tweet={tweet} />
+        ))}
+      </ul>
+      <Footer />
+    </React.Fragment>
+  );
+};
+export default Tweets;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'docs'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+};
